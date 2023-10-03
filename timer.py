2f1cdea972e18420a7eedf96322da9693eed5acc
Flora378 <flora.kwan@mail.utoronto.ca>
2023-10-03 14:24:55 -0400

    made a nerves of steel program from the timer program

--- timer.py
+++ timer.py
@@ -1,23 +1,17 @@
 """
-This program is used as an example for MGTC28.
-timer.py is a simple Python script that will allow user to set timer duration.
-Upon timer expiry, user will see the time up meme and sound notification.
-timer.py uses the time library to help keep track of time
+This program is an edit of the example used for MGTC28.
+timer.py is a simple Python script that was changed to play the nerves of steel game.
 """
 
-
-# This program is timer that counts down
-
+# This program is used for the nerves of steel game
 
 import time # The time library has a sleep function that will pause the script for a specifized amount of time
-from PIL import Image # the pillow library makes it easy to display images 
+import random # The random library has functions to generate a random number
 
-im = Image.open("times-up.jpeg")
+print ("Players stand")
 
-# ask user to enter desired countdown time
-set_time = int(input("Please set your timer in seconds: "))
+random_time = random.randint(5,25) #generate random integer between 5 and 25
 
-time.sleep(set_time)
+time.sleep(random_time) #sleeps program for the random time generated
 
-im.show()
-
+print ("Time up. Last to sit down wins")
